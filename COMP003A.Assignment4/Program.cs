6a5d6643158d7ceba55308014000ce6546cd56cd
flores-97 <floresvictor15@gmail.com>
2025-02-06 16:17:59 -0800

    started over. menu option 1 set

--- COMP003A.Assignment4/Program.cs
+++ COMP003A.Assignment4/Program.cs
@@ -4,58 +4,65 @@ Course: COMP-003A
 Faculty: Jonathon Cruz
 Purpose: Inventory management application with a minimum of 10 elements in the collection.
 */
+using System.Xml.Serialization;
+
 namespace COMP003A.Assignment4
 {
     internal class Program
     {
         static void Main(string[] args)
-
-            
-            
         {
+           
             Console.WriteLine("Welcome to the Inventory Management System!");
-            int done;
             int choice;
+            int done;
+
+           
+
             while (true)
             {
-                Console.WriteLine("Choose data storage type: ");
-                Console.WriteLine("1. Array\n2. String\n");
+                Console.WriteLine("Inventory Management System Menu: ");
+                Console.WriteLine("1. Add a Product");
+                Console.WriteLine("2. Update Product Quantity");
+                Console.WriteLine("3. View Inventory Summary");
+                Console.WriteLine("4. Exit");
+
                 Console.Write("Enter your choice: ");
+
                 choice = int.Parse(Console.ReadLine());
 
-                if (choice >= 3)
-                {
-                    Console.WriteLine("Invalid entry. Select '1' or '2'.");
-                }
+
+
+                if (choice == 4) break;
+
+                
 
                 if (choice == 1)
-                { //enteries for array below
-                    Menu();
+                {
+                    List<string> productNames = new List<string>();
+                    List<int> productQuantities = new List<int>();
 
-                    Console.Write("Enter '4' when done: ");
-                    done = int.Parse(Console.ReadLine());
+                    foreach (string name in productNames)
+                        {
+                            Console.WriteLine(name);
+                        }
+                    Console.Write("\nEnter product name: ");
+                    productNames.Add(Console.ReadLine());
+                    Console.Write("Enter proudct quanity: ");
+                    productQuantities.Add(int.Parse(Console.ReadLine()));
+                    Console.WriteLine("Product added successfully!\n");
 
-                    if (choice == done) break;
                 }
-
+                
                 if (choice == 2)
-                {//enteries for list
-                    Menu();
+                {
 
-                    Console.Write("Enter '4' when done: ");
-                    done = int.Parse(Console.ReadLine());
-
-                    if (choice == done) break;
                 }
 
-            }
-          static void Menu()
-            {
-                    Console.WriteLine("Inventory Management System Menu: ");
-                    Console.WriteLine("1. Add a Product");
-                    Console.WriteLine("2. Update Product Quantity");
-                    Console.WriteLine("3. View Inventory Summary");
-                    Console.WriteLine("4. Exit");
+                if (choice == 3)
+                {
+                 
+                }
             }
         }
     }
